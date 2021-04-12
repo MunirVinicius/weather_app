b61ffe312af6b0c51d01c1684b03ff36f89f2fd3
MunirVinicius <munir.vinicius11@gmail.com>
2021-04-12 02:02:38 -0300

    Object Destructuring ES6

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,16 +2,16 @@ const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback)=>{
     const url = `http://api.weatherstack.com/current?access_key=97aae6d1a96cc1eb03f5929cd58c51e7&query=` + latitude +`,`+longitude
-    request({url:url, json:true}, (error, response)=>{
+    request({url, json:true}, (error, {body})=>{
         if(error){
             callback('Unable to connect to WeatherStack services.')
         }
-        else if(response.body.error){
-            callback(response.body.error.info)
+        else if(body.error){
+            callback(body.error.info)
         }
         else{
-            const fore_cast = response.body.current.weather_descriptions[0];
-            callback(undefined, fore_cast + ' , the temperature is: ' + response.body.current.temperature + '  celcius, and the wind speed is: ' + response.body.current.wind_speed)
+            const fore_cast = body.current.weather_descriptions[0];
+            callback(undefined, fore_cast + ' , the temperature is: ' + body.current.temperature + '  celcius degress, and the wind speed is: ' + body.current.wind_speed)
         }
     })
 }
