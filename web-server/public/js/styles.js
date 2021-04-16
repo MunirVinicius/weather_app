e52c450e3dc5a4238a62880080843bfb83fa9dec
MunirVinicius <munir.vinicius11@gmail.com>
2021-04-16 02:00:52 -0300

    Wiring Everything Up

--- web-server/public/js/styles.js
+++ web-server/public/js/styles.js
@@ -1,15 +1,21 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const dataLocation = document.querySelector('#messageOne')
+const dataForecast = document.querySelector('#messageTwo')
+
+
 weatherForm.addEventListener('submit', (event)=>{
     event.preventDefault()
     const location = search.value
+    dataLocation.textContent = 'Loading ...'
+    dataForecast.textContent = ''
     fetch('http://localhost:3000/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
-                console.log(data.error)
+                dataLocation.textContent = data.error
             } else{
-                console.log(data.location)
-                console.log(data.forecast)
+                dataLocation.textContent =data.location
+                dataForecast.textContent = data.forecast
             }
         })
     })        
