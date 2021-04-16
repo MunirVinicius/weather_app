d87907eb1b971b9c8df7b5f3e4293ca086d558d7
MunirVinicius <munir.vinicius11@gmail.com>
2021-04-15 23:37:16 -0300

    API Getting The Request From Url

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,11 +40,29 @@ app.get('/help',(req,res)=>{
 })
 
 app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
     res.send({
         forecast:'its raining today',
-        location: 'Rio de Janeiro'
+        location: 'Phil',
+        address: req.query.address
     })
 })
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products: []
+    })
+})
+
 
 app.get('/help/*', (req,res)=>{
     res.render('404',{
